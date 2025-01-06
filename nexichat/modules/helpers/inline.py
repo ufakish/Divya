8e411eb96a7d8c43d4744236c304d289327bed1c
ufakish <phalishort@gmail.com>
2025-01-06 04:09:20 +0000

    Update inline.py

--- nexichat/modules/helpers/inline.py
+++ nexichat/modules/helpers/inline.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 from config import SUPPORT_GRP, UPDATE_CHNL
-from Mickey import OWNER
+from nexichat import OWNER
 from nexichat import nexichat
 
 DEV_OP = [
