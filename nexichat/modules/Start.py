a67f1af6ce639ebd989ec38b54ec9c00c2111320
ufakish <phalishort@gmail.com>
2025-01-07 06:09:12 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -75,7 +75,7 @@ IMG = [
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    _mongo_async_ = AsyncIOMotorClient(MONGO_URL) 
     mongodb = _mongo_async_.Yukki
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
